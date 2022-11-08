4d61bcd5f52bae42c470e678b360770e1333b5a2
Prem Lamsal <premlamsal2@gmail.com>
2022-11-08 20:57:04 +0545

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Getting Started with Nirvoice
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is developed for creating Inoices using React Js and Laravel API.
 
 ## Available Scripts
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,8 +24,9 @@ export default function Login() {
         //                 console.error(error);
         //             });
         //     });
+        // console.log(email.target.value);
+        // console.log(password.target.value);
         console.log(email);
-        console.log(password);
 
 
 
@@ -33,11 +34,11 @@ export default function Login() {
 
     return (
         <div>
-            <div class="h-screen flex">
-                <div class="hidden lg:flex w-full lg:w-1/2 login_img_section
+            <div className="h-screen flex">
+                <div className="hidden lg:flex w-full lg:w-1/2 login_img_section
           justify-around items-center">
                     <div
-                        class=" 
+                        className=" 
                   bg-black 
                   opacity-20 
                   inset-0 
@@ -45,37 +46,38 @@ export default function Login() {
                     >
 
                     </div>
-                    <div class="w-full mx-auto px-20 flex-col items-center space-y-6">
-                        <h1 class="text-white font-bold text-4xl font-sans">{process.env.REACT_APP_NAME}</h1>
-                        <p class="text-white mt-1">The simplest app to use</p>
-                        <div class="flex justify-center lg:justify-start mt-6">
-                            <Link to="#" class="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2">Get Started</Link>
+                    <div className="w-full mx-auto px-20 flex-col items-center space-y-6">
+                        <h1 className="text-white font-bold text-4xl font-sans">{process.env.REACT_APP_NAME}</h1>
+                        <p className="text-white mt-1">The simplest app to use</p>
+                        <div className="flex justify-center lg:justify-start mt-6">
+                            <Link to="#" className="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2">Get Started</Link>
                         </div>
                     </div>
                 </div>
-                <div class="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
-                    <div class="w-full px-8 md:px-32 lg:px-24">
-                        <form class="bg-white rounded-md shadow-2xl p-5">
-                            <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
-                            <p class="text-sm font-normal text-gray-600 mb-8">Welcome Back</p>
-                            <div class="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+                    <div className="w-full px-8 md:px-32 lg:px-24">
+                        <form className="bg-white rounded-md shadow-2xl p-5">
+                            <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
+                            {email}-{password}
+                            <p className="text-sm font-normal text-gray-600 mb-8">Welcome Back</p>
+                            <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                                 </svg>
-                                <input id="email" class=" pl-2 w-full outline-none border-none" type="email" name="email" placeholder="Email Address" onChange={setEmail}/>
+                                <input id="email" className=" pl-2 w-full outline-none border-none" type="email" name="email" placeholder="Email Address" onChange={(e)=>setEmail(e.target.value)}/>
                             </div>
-                            <div class="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                 </svg>
-                                <input class="pl-2 w-full outline-none border-none" type="password" name="password" id="password" placeholder="Password" onChange={setPassword}/>
+                                <input className="pl-2 w-full outline-none border-none" type="password" name="password" id="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
 
                             </div>
-                            <button type="submit" class="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2" onClick={Login}>Login</button>
-                            <div class="flex justify-between mt-4">
-                                <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Forgot Password ?</span>
+                            <button type="submit" className="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2" onClick={Login}>Login</button>
+                            <div className="flex justify-between mt-4">
+                                <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Forgot Password ?</span>
 
-                                <Link to="#" class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Don't have an account yet?</Link>
+                                <Link to="#" className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Don't have an account yet?</Link>
                             </div>
 
                         </form>
